b1e72b5237a1d3b404501363aa72c3ae907e51b0
Aaron Barber <asbarber@umich.edu>
2014-09-30 19:16:41 -0400

    Included Point interface

---
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/com/webs/michael_ray/meetingofminds/logic/Point.java
@@ -0,0 +1,24 @@
+package com.webs.michael_ray.meetingofminds.com.webs.michael_ray.meetingofminds.logic;
+
+/**
+ * Created by asb on 30/09/14.
+ */
+public interface Point {
+
+    public String getCat();
+
+    public String getName();
+
+    public int getIcon();
+
+    public int getDir();
+
+    public double getDist();
+
+    public double getRate();
+
+    public boolean getFav();
+
+    public void setFav();
+
+}
