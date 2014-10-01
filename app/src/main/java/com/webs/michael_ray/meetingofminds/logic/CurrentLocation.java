83ef9eedda03a6723a69fd38c50bcca301d891ff
Aaron Barber <asbarber@umich.edu>
2014-09-30 23:33:49 -0400

    Point direction calculations

---
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/logic/CurrentLocation.java
@@ -0,0 +1,13 @@
+package com.webs.michael_ray.meetingofminds.logic;
+
+import android.location.Location;
+
+/**
+ * Created by asb on 30/09/14.
+ */
+public class CurrentLocation {
+    public static Location getCurrentLocation(){
+        //TODO
+        return null;
+    }
+}
