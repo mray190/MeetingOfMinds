89430921b469ac3145e723810099ccbab85b0406
Michael Ray <mray190@gmail.com>
2014-10-01 00:31:10 -0400

    Added dialog box for dropping a pin

--- app/src/main/java/com/webs/michael_ray/meetingofminds/Home.java
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/Home.java
@@ -1,25 +1,39 @@
 package com.webs.michael_ray.meetingofminds;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.webs.michael_ray.meetingofminds.adapters.TabsAdapter;
+import com.webs.michael_ray.meetingofminds.logic.DatabaseManager;
 
 public class Home extends FragmentActivity implements ActionBar.TabListener {
 
     private ViewPager mPager;
     private TabsAdapter mPagerAdapter;
     private ActionBar actionBar;
+    private FragmentManager fm;
+    private SharedPreferences prefs;
 
     private void managePageNavigation() {
         mPager = (ViewPager) findViewById(R.id.pager);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Sets an action bar with tabs
         actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -27,7 +41,7 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
         actionBar.setDisplayHomeAsUpEnabled(true);
         //actionBar.color
 
-        FragmentManager fm = getSupportFragmentManager();
+        fm = getSupportFragmentManager();
 
         mPagerAdapter = new TabsAdapter(fm);
         mPager.setAdapter(mPagerAdapter);
@@ -55,10 +69,44 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         managePageNavigation();
+        login();
     }
 
     private void login() {
+        try {
+            int id = DatabaseManager.dm.authUser(prefs.getString("pref_username",""),prefs.getString("pref_password",""));
+            Toast.makeText(this, "ID: " + Integer.toString(id), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Fail", Toast.LENGTH_LONG).show();
+        }
+    }
 
+    public void addPoint(View view) {
+        AddPoint dialog = new AddPoint();
+        dialog.show(getFragmentManager(), "dialog");
+    }
+
+    public static class AddPoint extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            builder.setView(inflater.inflate(R.layout.dial_add, null))
+                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            String category = ((EditText)getActivity().findViewById(R.id.cat_add)).getText().toString();
+                            String name = ((EditText)getActivity().findViewById(R.id.name_add)).getText().toString();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            AddPoint.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
     }
 
     @Override
