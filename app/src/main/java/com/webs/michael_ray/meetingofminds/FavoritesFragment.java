cd4d2cffbaca3efabd9783154f131b851e8038c5
Michael Ray <mray190@gmail.com>
2014-10-01 02:34:31 -0400

    Started designating AsyncTask for pulling info from the database

--- app/src/main/java/com/webs/michael_ray/meetingofminds/FavoritesFragment.java
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/FavoritesFragment.java
@@ -1,19 +1,25 @@
 package com.webs.michael_ray.meetingofminds;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ListView;
 
 import com.webs.michael_ray.meetingofminds.adapters.PointNearAdapter;
+import com.webs.michael_ray.meetingofminds.logic.DatabaseManager;
 import com.webs.michael_ray.meetingofminds.logic.Point;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class FavoritesFragment extends ListFragment {
-    private PointNearAdapter pAdapter;
-    private ArrayList<Point> points;
+    private int userID;
+
+    public FavoritesFragment(int userID) {
+        this.userID = userID;
+    }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +29,34 @@ public class FavoritesFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getListView().setCacheColorHint(Color.TRANSPARENT);
-        points = new ArrayList<Point>();
-        pAdapter = new PointNearAdapter(getActivity(), R.layout.row_near, points);
-        setListAdapter(pAdapter);
+        GetFav getFav = new GetFav();
+        getFav.execute();
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
 
+    }
+
+    private class GetFav extends AsyncTask<Void, Integer, Void> {
+        private PointNearAdapter pAdapter;
+        private ArrayList<Point> points;
+        @Override
+        protected Void doInBackground(Void...params) {
+            try {
+                points = DatabaseManager.dm.findFavorites(userID);
+            } catch (SQLException e) {
+                points = new ArrayList<Point>();
+            }
+            return null;
+        }
+        protected void onProgressUpdate(Integer...progress) {
+
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            pAdapter = new PointNearAdapter(getActivity(), R.layout.row_near, points);
+            setListAdapter(pAdapter);
+        }
     }
 }
