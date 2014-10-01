90e78e0d89825c7f95d801dd2a93771567483d14
Aaron Barber <asbarber@umich.edu>
2014-10-01 04:25:45 -0400

    CategoryManager changes

--- app/src/main/java/com/webs/michael_ray/meetingofminds/logic/CategoryManager.java
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/logic/CategoryManager.java
@@ -5,13 +5,14 @@ package com.webs.michael_ray.meetingofminds.logic;
  */
 public class CategoryManager {
 
-    public static int resource(String category){
+
+//    public static ArrayList<ArrayList<String>> read(){
 //        //Gets the data stream
 //        Resources res = getResources();
 //        InputStream in = res.openRawResource(R.raw.category);
 //
-//
-//
+//        //Data to return
+//        ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
 //
 //        //Reads the data line by line
 //        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -20,16 +21,24 @@ public class CategoryManager {
 //            while ((line = reader.readLine()) != null) {
 //                String[] vals = line.split(",");
 //
-//                if (vals[0].equals(category)){
+//                ArrayList<String> tmp = new ArrayList<String>();
 //
+//                for (String val: vals){
+//                    tmp.add(val);
 //                }
+//
+//                arr.add(tmp);
 //            }
 //
 //            reader.close();
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+//
+//        return arr;
+//    }
 
+    public static int resource(String subCategory){
         return 0;
     }
 }
