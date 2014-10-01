d32cbc0bcfc634be4f2414a9ebd2073469c9a635
Shane Schulte <schulte.shane@gmail.com>
2014-10-01 05:42:24 -0400

    Map icons

--- app/src/main/java/com/webs/michael_ray/meetingofminds/Maps.java
+++ app/src/main/java/com/webs/michael_ray/meetingofminds/Maps.java
@@ -62,7 +62,7 @@ public class Maps extends FragmentActivity {
         }
     }
 
-
+    //t
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
